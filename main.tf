2bcbf1e693455cee4ea29a54a80288c86fec9889
Illia Bilous <krabbys1111@gmail.com>
2025-09-07 00:30:32 +0300

    Format main.tf for better readability and consistency

--- main.tf
+++ main.tf
@@ -9,23 +9,23 @@
 terraform {
   required_version = ">= 1.8.4"
 
-required_providers {
-  azurerm = {
-  source  = "hashicorp/azurerm"
-  version = ">= 4.41.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.41.0"
+    }
   }
-}
 
-backend "azurerm" {
-  resource_group_name  = "tfstate"
-  storage_account_name = "matetfstatestorageac52"
-  container_name       = "tfstate"
-  key                  = "terraform.tfstate"
-  use_oidc             = true
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "matetfstatestorageac52"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
 
 provider "azurerm" {
-features {}
-use_oidc = true
+  features {}
+  use_oidc = true
 }
